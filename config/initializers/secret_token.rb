f6634d24a062d56e8898aeed613ab816195479da
spencer <spkw888@gmail.com>
2012-12-11 12:27:21 -0500

    All the new stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swrails::Application.config.secret_token = 'a031c3fcbb50065e8c1d4d12326c65a161581e343f5ac6e569eddfa6d15563cb86b91eadd53658787cb897d7fdf40eb2ff26a084ca57000dbd221d3453a4e2c5'
